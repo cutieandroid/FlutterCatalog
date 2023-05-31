e9a96ef7d4851723de29f4a374bc29907e0685bf
harsh <20102161.harshmali@gmail.com>
2023-05-31 10:13:53 +0530

    used an api to get the hosted data from the internet and not from a local json file using http dependency

--- lib/Pages/homepage.dart
+++ lib/Pages/homepage.dart
@@ -1,11 +1,14 @@
 import 'dart:convert';
-
+import 'package:http/http.dart' as http;
+import 'package:catalog/core/MyStore.dart';
+import 'package:catalog/models/cartmodel.dart';
 import 'package:catalog/models/catalogs.dart';
 import 'package:catalog/utils/MyRoutes.dart';
 import 'package:catalog/widgets/MyThemes.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:velocity_x/velocity_x.dart';
 
 
 import '../widgets/HomePageWidgets/catalogheader.dart';
@@ -16,44 +19,61 @@ class Homepage extends StatefulWidget {
   State<Homepage> createState() => _HomepageState();
 }
 
-class _HomepageState extends State<Homepage> {
+class _HomepageState extends State<Homepage>{
   //whenever this homepage widget is initialized we will get the data from our json file
   @override
   void initState() {
     super.initState();
     loaddata();
   }
+  final url="https://api.jsonbin.io/v3/b/6475fc788e4aa6225ea6a43e";
 
   loaddata() async {
     await Future.delayed(Duration(seconds: 2));
-    var catalogjson = await rootBundle.loadString("assets/files/catalog.json");
+    final response= await http.get(Uri.parse(url));
+    //var catalogjson = await rootBundle.loadString("assets/files/catalog.json");
+    var catalogjson=response.body;
     var decodedata = jsonDecode(catalogjson);
-    var productsdata = decodedata["products"];
+    //var productsdata=decodeddata["products"];
+    var data = decodedata["record"];
+    var productsdata=data["products"];
     //we have a class catalog models wherein we have a list called items, so we need to first fill that list
     //then we can use that list in fetch method
-    CatalogModel.items = List.from(productsdata)
-        .map<Item>((item) => Item.fromMap(item))
-        .toList();
-    setState(() {});
+    if (productsdata!= null) {
+      CatalogModel.items = List.from(productsdata)
+          .map<Item>((item) => Item.fromMap(item))
+          .toList();
+      setState(() {});
+    } else {
+      // do something else
+    }
+
+
   }
 
   @override
   Widget build(BuildContext context) {
     //final dumylist=List.generate(50, (index) => CatalogModel.items[0]);
+    final cart= (VxState.store as MyStore).cart;
 
     return (
      Scaffold(
 
 
-       floatingActionButton: FloatingActionButton(
-         onPressed: () {
+       floatingActionButton: VxBuilder(
+         builder: (BuildContext context, store, VxStatus? status) =>
+           FloatingActionButton(
+             onPressed: () {
 
-           Navigator.pushNamed(context, MyRoutes.cartroute);
+               Navigator.pushNamed(context, MyRoutes.cartroute);
 
-         },
-         backgroundColor: MyTheme.darkbluish,
-         child: const Icon(CupertinoIcons.cart,color: Colors.white),
+             },
+             backgroundColor: MyTheme.darkbluish,
+             child: const Icon(CupertinoIcons.cart,color: Colors.white),
 
+           ).badge(color: Colors.red,count: cart.items.length,size: 25,textStyle: TextStyle(fontSize: 20)),
+
+         mutations: {AddMutation,RemoveMutation},
        ),
 
        backgroundColor: Theme.of(context).canvasColor ,
